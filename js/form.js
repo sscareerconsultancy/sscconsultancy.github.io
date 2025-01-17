4d751fdb9ccb4c4a8c8903443fd14c54858c13bd
sscareerconsultancy <consultancysscareer@gmail.com>
2025-01-17 20:34:17 +0530

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,3 +1,4 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-          window.location.href = 'form.html';
-)}
+document.getElementById('submit-btn').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+    window.location.href = 'form.html'; // Redirect to the form.html page
+});
