e6ea57559f93458c6206d2c5fa1a0fb0b31a6248
sscareerconsultancy <consultancysscareer@gmail.com>
2025-01-19 00:01:03 +0530

    merge

---
+++ js/responsive.js
@@ -0,0 +1,39 @@
+/* Add this CSS to your stylesheet */
+
+/* General styles for mobile devices */
+@media only screen and (max-width: 600px) {
+    #header, #footer {
+      padding: 10px;
+      text-align: center;
+    }
+    
+    .sf-menu {
+      display: flex;
+      flex-direction: column;
+      padding: 0;
+      margin: 0;
+      list-style: none;
+    }
+    
+    .sf-menu li {
+      margin: 5px 0;
+    }
+    
+    .grid_12 {
+      width: 100%;
+      padding: 0;
+    }
+    
+    .camera_caption {
+      font-size: 1.2em;
+    }
+    
+    .element h3 {
+      font-size: 1.1em;
+    }
+    
+    .footer_bottom, .copyright {
+      font-size: 0.8em;
+    }
+  }
+  
